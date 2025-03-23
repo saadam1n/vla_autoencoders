6cdc10d5a451a30557dfce1cf1776f311e74c5a4
Saad Amin <58188800+saadam1n@users.noreply.github.com>
2025-03-22 23:57:29 -0400

    Switch to mean/std normalization

--- data_stuff.py
+++ data_stuff.py
@@ -9,18 +9,43 @@ class ActionChunkDataset(torch.utils.data.Dataset):
 
         print(f"Traing data shape is {self.all_chunks.shape}")
 
-        sorted_chunks, _ = torch.sort(self.all_chunks.flatten(0, 1), dim=0)
+        if False:
+            sorted_chunks, _ = torch.sort(self.all_chunks.flatten(0, 1), dim=0)
 
-        lower = sorted_chunks[int(0.01 * sorted_chunks.shape[0])]
-        upper = sorted_chunks[int(0.99 * sorted_chunks.shape[0])]
-        range = upper - lower
+            lower = sorted_chunks[int(0.01 * sorted_chunks.shape[0])]
+            upper = sorted_chunks[int(0.99 * sorted_chunks.shape[0])]
+            range = upper - lower
 
-        print("Dataset statistics:")
-        print(f"\tlower: {lower}")
-        print(f"\tupper: {upper}")
-        print(f"\trange: {range}")
+            print("Dataset statistics:")
+            print(f"\tlower: {lower}")
+            print(f"\tupper: {upper}")
+            print(f"\trange: {range}")
 
-        self.all_chunks = (self.all_chunks - lower) / range
+            self.all_chunks = (self.all_chunks - lower) / range
+        else:
+            flattened_chunks = self.all_chunks.flatten(0, 1)
+            var, mean = torch.var_mean(flattened_chunks, dim=0)
+
+            std = torch.sqrt(var + 1e-5)
+
+
+
+            print("Dataset statistics:")
+            print(f"\tmean: {mean}")
+            print(f"\tvar : {var}")
+            print(f"\tstd : {std}")
+
+            mean = mean.view(1, 1, -1)
+            std = std.view(1, 1, -1)
+
+            self.all_chunks = (self.all_chunks - mean) / std
+
+            check_var, check_mean = torch.var_mean(self.all_chunks.flatten(0, 1))
+
+            print("Check stats:")
+            print(f"\tmean: {check_mean}")
+            print(f"\tvar : {check_var}")
+
 
         self.train_mode = False
 
